ec7bed84324db8ede51a58fe13dcf29b2ed4a170
Vishal Billa <vbilla@usc.edu>
2024-04-01 12:42:47 -0700

    revise main loop to only parse if sentence is GPRMC, improve debug outputs

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -11,8 +11,6 @@
 #include "soft_serial_dbg.h"
 #include "gps.h"
 
-#include <stdio.h>
-
 int main() {
 
     // Initialize UART communication
@@ -27,9 +25,17 @@ int main() {
 
     // Create string to hold data to send over software serial
     char data[100];
+    memset(data, 0, sizeof(data));
 
     // Create string to hold received NMEA sentence
     char nmea_received[85];
+    memset(nmea_received, 0, sizeof(nmea_received));
+
+    // Index to keep track of received NMEA sentence
+    uint8_t nmea_index = 0;
+
+    // Flag to indicate if we are receiving a GPRMC sentence
+    bool gprmc_sentence = false;
 
     // Turn on global interrupts
     sei();
@@ -45,20 +51,31 @@ int main() {
             if (c == '$') {
                 // Reset the received NMEA sentence
                 memset(nmea_received, 0, sizeof(nmea_received));
+                nmea_index = 0;
+                gprmc_sentence = false;
             }
 
             // Add the character to the received NMEA sentence
-            strncat(nmea_received, &c, 1);
+            nmea_received[nmea_index++] = c;
+
+            // Examine the first 6 characters of the NMEA sentence
+            if (nmea_index == 6) {
+                // Check if the NMEA sentence is a GPRMC sentence
+                if (strncmp(nmea_received, "$GPRMC", 6) == 0) {
+                    gprmc_sentence = true;
+                }
+            }
 
             // Check if the NMEA sentence is complete
-            if (c == '\n') {
+            if ((c == '\n') && gprmc_sentence) {
                 // Parse the NMEA sentence
                 GPS_parse_gprmc(&gps, nmea_received);
 
                 // Check if the GPS data is valid
                 if (GPS_is_valid(&gps)) {
                     // Create string to hold GPS data
-                    sprintf(data, "Time: %s, Lat: %s %s, Lon: %s %s, Speed: %s, Heading: %s\n", gps.time, gps.lat, gps.lat_dir, gps.lon, gps.lon_dir, gps.speed, gps.heading);
+                    sprintf(data, "Time: %s, Lat: %s %s, Lon: %s %s, Speed: %s, Heading: %s\n",
+                            gps.time, gps.lat, gps.lat_dir, gps.lon, gps.lon_dir, gps.speed, gps.heading);
 
                     // Send GPS data over software serial
                     dbg_send_string(data);
@@ -70,6 +87,8 @@ int main() {
                     // Send invalid GPS data over software serial
                     dbg_send_string("Invalid GPS data\n");
                 }
+            } else if (c == '\n') {
+                dbg_send_string("Not a GPRMC sentence\n");
             }
         }
     }
