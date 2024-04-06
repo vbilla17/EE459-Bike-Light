82d082483f823223ca7871a12357f1aa4b4f68d4
Vishal Billa <vbilla@usc.edu>
2024-04-05 20:02:19 -0700

    fix parsing mechanism, some cleanup

--- Firmware/include/gps.c
+++ Firmware/include/gps.c
@@ -1,40 +1,67 @@
 /**
  * @file gps.c
  * @author Vishal Billa (vbilla@usc.edu)
- * @brief 
- * @version 1.0
+ * @brief Implementation file for GPS parsing library
  * @date 2024-03-04
- * 
- * @copyright Copyright (c) 2024
- * 
+ *
  */
 
 #include "gps.h"
 
 void GPS_init(GPSData* gps) {
+    // Initialize all fields to 0
     memset (gps->time, 0, sizeof(gps->time));
     memset (gps->lat, 0, sizeof(gps->lat));
-    memset (gps->lat_dir, 0, sizeof(gps->lat_dir));
+    gps->lat_dir = '\0';
     memset (gps->lon, 0, sizeof(gps->lon));
-    memset (gps->lon_dir, 0, sizeof(gps->lon_dir));
+    gps->lon_dir = '\0';
     memset (gps->speed, 0, sizeof(gps->speed));
     memset (gps->heading, 0, sizeof(gps->heading));
     gps->valid = false;
+    return;
 }
 
 void GPS_parse_gprmc(GPSData* GPS, char* sentence) {
-    char mode_indicator = 0;
-    char pos_status = 0;
-    int scan_result = sscanf(sentence, "$GPRMC,%6s,%c,%9s,%c,%10s,%c,%7s,%6s,%*s,%*s,%*s,%c,%*s",
-                             GPS->time, &pos_status, GPS->lat, GPS->lat_dir, GPS->lon, GPS->lon_dir,
-                             GPS->speed, GPS->heading, &mode_indicator);
-    if ((scan_result == 9) && (pos_status == 'A') && (mode_indicator != 'N')) {
+    // Declare variables to hold parsed values, length needs to be larger than the max field length
+    char utc [15] = {0};
+    char status = '\0';
+    char lat [15] = {0};
+    char lat_dir = '\0';
+    char lon [15] = {0};
+    char lon_dir = '\0';
+    char speed [15] = {0};
+    char heading [15] = {0};
+    char mode = '\0';
+
+    // Check if the sentence is a valid RMC sentence
+    if (strncmp(sentence, "$GPRMC", 6) != 0) {
+        GPS->valid = false;
+        return;
+    }
+
+    // $GPRMC,utc,status,lat,N/S,lon,E/W,speed,hdg,date,mag,var,mode*cs
+    uint8_t fields = sscanf(sentence, "$GPRMC,%15[^,],%c,%15[^,],%c,%15[^,],%c,%15[^,],%15[^,],%*[^,],,,%c",
+           utc, &status, lat, &lat_dir, lon, &lon_dir, speed, heading, &mode);
+
+    // Check if we read all necessary fields, data is valid, and mode is not "Data not valid"
+    if ((fields == 9) && ((status == 'A') && (mode != 'N'))) {
+        // Copy parsed values to GPS struct and set valid flag
+        strncpy(GPS->time, utc, MAX_TIME_LEN);
+        strncpy(GPS->lat, lat, MAX_LAT_LEN);
+        GPS->lat_dir = lat_dir;
+        strncpy(GPS->lon, lon, MAX_LON_LEN);
+        GPS->lon_dir = lon_dir;
+        strncpy(GPS->speed, speed, MAX_SPEED_LEN);
+        strncpy(GPS->heading, heading, MAX_HEADING_LEN);
         GPS->valid = true;
     } else {
+        // Invalid data
         GPS->valid = false;
     }
+    return;
 }
 
 void GPS_invalidate(GPSData* gps) {
     gps->valid = false;
+    return;
 }
--- Firmware/include/gps.h
+++ Firmware/include/gps.h
@@ -1,21 +1,19 @@
 /**
  * @file gps.h
  * @author Vishal Billa (vbilla@usc.edu)
- * @brief 
- * @version 1.0
+ * @brief Header file for GPS parsing library
  * @date 2024-03-04
- * @
- * 
- * @copyright Copyright (c) 2024
- * 
- * @brief This AVR C library is used to interface with the Adafruit Ultimate GPS Breakout module.
- *        NMEA sentences are parsed and the latitude, longitude, speed, heading, and time 
- *        are extracted. Specifically, we are interested in RMC sentences.
+ *
+ * @details This library is used to parse data from the Adafruit Ultimate GPS Breakout module.
+ *        NMEA sentences are parsed and the latitude, longitude, speed, heading, and time
+ *        are extracted. Specifically, we are interested in GPRMC sentences.
+ *
  */
 
 #ifndef GPS_H
 #define GPS_H
 
+#include "soft_serial_dbg.h"
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
@@ -24,9 +22,7 @@
 // Define max length of NMEA fields
 #define MAX_TIME_LEN 6 // hhmmss
 #define MAX_LAT_LEN 9 // ddmm.mmmm
-#define MAX_LAT_DIR_LEN 1 // D
 #define MAX_LON_LEN 10 // dddmm.mmmm
-#define MAX_LON_DIR_LEN 1 // D
 #define MAX_SPEED_LEN 6 // kk.kkk
 #define MAX_HEADING_LEN 6 // ddd.dd
 
@@ -38,9 +34,9 @@ typedef struct {
     // Add 1 to all char arrays to include null termination
     char time[MAX_TIME_LEN + 1];
     char lat[MAX_LAT_LEN + 1];
-    char lat_dir[MAX_LAT_DIR_LEN + 1];
+    char lat_dir;
     char lon[MAX_LON_LEN + 1];
-    char lon_dir[MAX_LON_DIR_LEN + 1];
+    char lon_dir;
     char speed[MAX_SPEED_LEN + 1];
     char heading[MAX_HEADING_LEN + 1];
     bool valid;
@@ -49,7 +45,7 @@ typedef struct {
 /**
  * @brief Initializes values of GPS struct
  * 
- * @param gps 
+ * @param gps GPS object to initialize
  */
 void GPS_init(GPSData* gps);
 
@@ -57,15 +53,15 @@ void GPS_init(GPSData* gps);
  * @brief Parses received NMEA sentence and stores necessary
  *        info in GPS struct
  * 
- * @param gps 
- * @param sentence 
+ * @param gps Target object for data
+ * @param sentence Input GPRMC sentence
  */
 void GPS_parse_gprmc(GPSData* gps, char* sentence);
 
 /**
  * @brief Sets status of GPS data structure to invalid
  * 
- * @param gps 
+ * @param gps GPS object to invalidate
  */
 void GPS_invalidate(GPSData* gps);
 
